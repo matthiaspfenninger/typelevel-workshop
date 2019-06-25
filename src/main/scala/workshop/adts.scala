3fd8c4da94964a27aaf428983642c8acd69e4d00
Matthias Pfenninger <matthias.pfenninger@gmail.com>
2019-06-25 09:54:34 +0200

    Solutions for ADTs and Typeclasses (up to word count example)

--- src/main/scala/workshop/adts.scala
+++ src/main/scala/workshop/adts.scala
@@ -12,38 +12,47 @@ object adts {
   type Contact = Unit
 
   // Design a data type for a chess piece and its position on the chess board
-  type ChessPiece = Unit
+  type ChessPiece = (String, Integer)
+  //sealed trait Piece
+  //sealed trait Position
 
   // Write a function using pattern matching that takes a square and returns whether it can move there or not
 
   // Model a data type that stores time spans
-  type TimeSpan = Unit
+  type TimeSpan = (Integer, Integer)
 
   // Write a function that adds two TimeSpan values together
 
   // List all values of the type `Unit`
-  def allValuesUnit: Set[Unit] = ???
+  def allValuesUnit: Set[Unit] = Set( () )
 
   // List all values of the type `Nothing`
-  def allValuesNothing: Set[Nothing] = ???
+  def allValuesNothing: Set[Nothing] = Set()
 
   // List all values of the type `Boolean`
-  def allValuesBoolean: Set[Boolean] = ???
+  def allValuesBoolean: Set[Boolean] = Set(true, false)
 
   // List all values of the type `Size`
-  def allValuesSize: Set[Size] = ???
+  def allValuesSize: Set[Size] = Set(Small, Medium, Large)
 
   // List all values of the type `(Size, Boolean)`
-  def allValuesTuple: Set[(Size, Boolean)] = ???
+  def allValuesTuple: Set[(Size, Boolean)] = Set(
+    (Small, true), (Medium, true), (Large, true), 
+    (Small, false), (Medium, false), (Large, false))
 
   // List all values of the type `Either[Size, Boolean]`
-  def allValuesEither: Set[Either[Size, Boolean]] = ???
+  def allValuesEither: Set[Either[Size, Boolean]] = Set(
+    Left(Small), Left(Medium), Left(Large),
+    Right(false), Right(true)
+  )
 
   // List all values of the type `(Size, Unit)`
-  def allValuesTupleUnit: Set[(Size, Unit)] = ???
+  def allValuesTupleUnit: Set[(Size, Unit)] = Set((Small, ()), (Medium, ()), (Large, ()))
 
   // List all values of the type `Either[Boolean, Nothing]`
-  def allValuesEitherNothing: Set[Either[Boolean, Nothing]] = ???
+  def allValuesEitherNothing: Set[Either[Boolean, Nothing]] = Set(
+    Left(true), Left(false)
+  )
 
 
   //The Law of Iso:
@@ -56,25 +65,60 @@ object adts {
   }
 
   //think about what the Size of `A` is and what the size of `(A,Unit)` is. What does this mean?
-  def tupleUnit[A]:Iso[A,(A,Unit)] = ???
-  //the same as above. Compare and contrast with how we saw `Either[A,B]` and `(A,B)` behave in terms of resulting size and the size of `Nothing` and `Unit`
-  def eitherNothing[A]:Iso[A,Either[A,Nothing]] = ???
+  def tupleUnit[X]:Iso[X,(X,Unit)] = Iso((x:X) => (x, ())) (tpl => tpl._1)
 
+  //the same as above. Compare and contrast with how we saw `Either[A,B]` and `(A,B)` behave in terms of resulting size and the size of `Nothing` and `Unit`  
+  def eitherNothing[X]:Iso[X,Either[X,Nothing]] = Iso(
+    (x:X) => Left(x): Either[X, Nothing]) (_ match { // you have to tell scala here that it is an Either[X, Nothing], otherwise it will pick the most specific type which would be Left[X]
+      case Left(x) => x
+      case Right(_) => ??? // this code can never be reached, hence the ??? are actually allowed! would be "Absurd" in Haskell. you could also do a throw error here
+    })
+
+  // what you often have in good libraries is helper objects like this which which enable you to leave out the Either[...] above
+  /*
+  object eitherNothing {
+    def left[X](x:X):Either[X, Nothing] = Left(x)
+  }
+  */
+
+  def eitherNothingViaFold[X]:Iso[X,Either[X,Nothing]] = Iso(
+    (x:X) => Left(x): Either[X, Nothing]) (_.fold(l => l, r => ???)) // is the same as the pattern matching above, just via fold
+  
 
   //OPTIONAL: If you want to convince yourself further about those properties there's a couple more proofs we can write
   //These get progressively harder and need deeper insight into what's going on.
 
-  def asocTuple[A,B,C]:Iso[((A,B),C), (A,(B,C))] = ???
-  def asocEither[A,B,C]:Iso[Either[Either[A,B],C], Either[A,Either[B,C]]] = ???
+  def asocTuple[A,B,C]:Iso[((A,B),C), (A,(B,C))] = Iso((tpl: ((A, B), C)) => ((tpl._1._1), (tpl._1._2, tpl._2))) (tpl => ((tpl._1, tpl._2._1), tpl._2._2))
+  
+  def asocEither[A,B,C]:Iso[Either[Either[A,B],C], Either[A,Either[B,C]]] = Iso( (_:Either[Either[A,B],C]) match {
+    case Left(Left(a)) => Left(a)
+    case Left(Right(b)) => Right(Left(b))
+    case Right(c) => Right(Right(c))
+  }) (_ match {
+    case Left(a) => Left(Left(a))
+    case Right(Left(b)) => Left(Right(b))
+    case Right(Right(c)) => Right(c)
+  })
 
-  def dist[A,B,C]:Iso[Either[(A,B), (A,C)], (A, Either[B,C])] = ???
+  def dist[A,B,C]:Iso[Either[(A,B), (A,C)], (A, Either[B,C])] = Iso((_: Either[(A,B), (A,C)]) match {
+    case Left((a,b)) => (a, Left(b))
+    case Right((a,c)) => (a, Right(c))
+  }) (_ match {
+    case (a, Left(b)) => Left((a, b))
+    case (a, Right(c)) => Right((a, c))
+  })
 
   sealed trait SizeTwo
   final case object One extends SizeTwo
   final case object Two extends SizeTwo
 
   //Arbitraty proofs can also be done
-  def optEither[A]:Iso[Option[A],Either[A,Unit]]=???
+  // map None <-> Right(()) and Some(A) <-> Left(A)
+  def optEither[X]:Iso[Option[X],Either[X,Unit]]= Iso((x:Option[X]) => 
+    x.fold[Either[X, Unit]](Right(()))(l => Left(l))) (_.fold(l => Option(l), r => Option.empty))
+  
+  // could also write Some(l) instead of Option(l), and None instead of Option.empty (but type inference is easier when done like above)
+  // nice thing: Option(something) gives you either back Some(something) or None if the something was Null, so use that constructor in productive code
 
   //Think about the sizes involved
   def arbitraryProof: Iso[Either[Either[Unit, Boolean], Either[Unit, Size]], Either[SizeTwo, Either[Boolean, Size]]] = Iso(
--- src/main/scala/workshop/typeclasses.scala
+++ src/main/scala/workshop/typeclasses.scala
@@ -12,7 +12,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object typeclasses {
 
-  //Show
+  // SHOW
+
+  // NOTE: show is meant to make things human readable, NOT to serialize stuff. only to be used for human debugging etc.
 
   @typeclass trait Show[A] {
     def show(a: A): String
@@ -26,14 +28,23 @@ object typeclasses {
     def show(a: String): String = a
   }
 
-  implicit def showChessPiece: Show[ChessPiece] = ???
+  implicit def showChessPiece: Show[ChessPiece] = new Show[ChessPiece] {
+    def show(a: ChessPiece): String = a._1.toString + " is on field " + a._2.toString
+  }
 
-  implicit def showOption[A: Show]: Show[Option[A]] = ???
+  // if you have a trait with only one function definition (which we have with Show above), you can also just
+  // define your implementation lambda style with Scala 2.12 (SAM)
+  implicit def showOption[A: Show]: Show[Option[A]] = option => option.fold("None")(x => Show[A].show(x))
 
 
+  // a Show function for tuples where each element has it's own Show
+  implicit def showTpl[A: Show, B:Show]: Show[(A,B)] = tpl => "(" + tpl._1 + ", " + tpl._2 + ")"
+
+  // a Show function for Lists where each element has it's own Show
+  implicit def showLst[A: Show]: Show[List[A]] = lst => lst.map(Show[A].show).mkString("[", ",", "]")
 
 
-  //Eq
+  // EQ
 
   @typeclass trait Eq[A] {
     def eqv(x: A, y: A): Boolean
@@ -48,15 +59,24 @@ object typeclasses {
     def eqv(x: String, y: String): Boolean = x.equals(y)
   }
 
-  implicit def eqOption[A: Eq]: Eq[Option[A]] = ???
+  implicit def eqOption[A: Eq]: Eq[Option[A]] = (x, y) => (x, y) match {
+    case (Some(a), Some(b)) => a === b
+    case (None, None) => true
+    case (None, _) => false
+    case (_, None) => false
+  }
 
-  implicit def eqEither[A: Eq, B: Eq]: Eq[Either[A, B]] = ???
+  implicit def eqEither[A: Eq, B: Eq]: Eq[Either[A, B]] = (x, y) => (x, y) match {
+    case (Left(a1), Left(a2)) => a1 === a2
+    case (Right(b1), Right(b2)) => b1 === b2
+    case _ => false 
+  }
+  // be careful with default pattern matches! in a productive setup where you have an evolving data model
+  // don't use it as you will have an exhaustive match ANYTIME and you might to forget to cover new cases
 
 
 
-
-
-  //Monoid
+  // MONOID
 
   @typeclass trait Monoid[A] {
     def empty: A
@@ -71,53 +91,130 @@ object typeclasses {
     def combine(x: Int, y: Int): Int = x + y
   }
 
-  implicit def stringMonoid: Monoid[String] = ???
+  implicit def stringMonoid: Monoid[String] = new Monoid[String] {
+    def empty: String = ""
 
-  implicit def timespanMonoid: Monoid[TimeSpan] = ???
+    def combine(x: String, y:String): String = x + y
+  }
 
-  implicit def listMonoid[A]: Monoid[List[A]] = ???
+  implicit def timespanMonoid: Monoid[TimeSpan] = new Monoid[TimeSpan] {
+    def empty: TimeSpan = new TimeSpan(0, 0)
+
+    def combine(x: TimeSpan, y: TimeSpan): TimeSpan = new TimeSpan(x._1 + y._1, x._2 + y._2)
+  }
+
+  implicit def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    def empty: List[A] = List.empty
+
+    def combine(x: List[A], y: List[A]): List[A] = x ++ y
+  }
 
   // The intMonoid further up uses `addition` as its method of combination, but that is not the only monoid for `Int`!
   // We can also define one for multiplication, though if we just define it for `Int` again the compiler won't know which to pick
   // What we can do instead is define a small wrapper that allows us to multiply
   case class Mult(value: Int)
 
-  implicit def multMonoid: Monoid[Mult] = ???
+  implicit def multMonoid: Monoid[Mult] = new Monoid[Mult] {
+    def empty: Mult = Mult(1)
 
-  def combineAll[A: Monoid](list: List[A]): A = ???
+    def combine(x: Mult, y: Mult): Mult = Mult(x.value * y.value)
+  }
 
-  def foldMap[A, B: Monoid](list: List[A])(f: A => B): B = ???
+  // foldRight is stack safe! use it wherever possible
+  // beautiful simple way
+  def combineAll[A: Monoid](list: List[A]): A = list.foldRight(Monoid[A].empty)(Monoid[A].combine)
 
-  implicit def tupleMonoid[A: Monoid, B: Monoid]: Monoid[(A, B)] = ???
+  // manually handrolled way with pattern matching
+  def combineAllHandrolled[A: Monoid](list: List[A]): A = list match {
+    case Nil => Monoid[A].empty
+    case (head :: Nil) => head
+    case (head :: tl) => Monoid[A].combine(head, combineAllHandrolled(tl))
+  }
 
-  implicit def tuple3Monoid[A: Monoid, B: Monoid, C: Monoid]: Monoid[(A, B, C)] = ???
+  // again the same, but via our own foldMap below
+  def combineAllViaFold[A: Monoid](list: List[A]): A = foldMap(list)(identity)
+  
+  // apply a map A=>B on every element of type A, and then fold the B elements
+  def foldMap[A, B: Monoid](list: List[A])(f: A => B): B = list.foldRight(Monoid[B].empty)((x, y) => Monoid[B].combine(f(x), y))
 
-  implicit def mapMonoid[A, B: Monoid]: Monoid[Map[A, B]] = ???
+  // tupleMonoid
+  implicit def tupleMonoid[A: Monoid, B: Monoid]: Monoid[(A, B)] = new Monoid[(A, B)] {
+    def empty: (A, B) = (Monoid[A].empty, Monoid[B].empty)
 
-  implicit def futureMonoid[A: Monoid]: Monoid[Future[A]] = ???
+    def combine(x: (A, B), y: (A, B)): (A, B) = (Monoid[A].combine(x._1, y._1), Monoid[B].combine(x._2, y._2))
+  }
+
+  // tuple3Monoid
+  implicit def tuple3Monoid[A: Monoid, B: Monoid, C: Monoid]: Monoid[(A, B, C)] = new Monoid[(A, B, C)] {
+    def empty: (A, B, C) = (
+      Monoid[A].empty, 
+      Monoid[B].empty, 
+      Monoid[C].empty
+      )
+
+    def combine(x: (A, B, C), y: (A, B, C)): (A, B, C) = (
+      Monoid[A].combine(x._1, y._1), 
+      Monoid[B].combine(x._2, y._2), 
+      Monoid[C].combine(x._3, y._3)
+      )
+  }
+
+  // mapMonoid
+  implicit def mapMonoid[A, B: Monoid]: Monoid[Map[A, B]] = new Monoid[Map[A, B]] {
+    def empty: Map[A,B] = Map.empty
+
+    def combine(x: Map[A, B], y: Map[A, B]): Map[A, B] = y.foldRight(x)(
+      (tpl, map) => map + (
+        tpl._1 -> map
+          .get(tpl._1)
+          .fold(tpl._2)(_ |+| tpl._2)
+        )
+    )
+  }
+
+  implicit def futureMonoid[A: Monoid]: Monoid[Future[A]] = new Monoid[Future[A]] {
+    def empty: Future[A] = Future.successful(Monoid[A].empty)
+
+    // this combine makes the execution of x and y sequential, because 'for' is desugared to a flatMap and flatMap only allows sequential operations 
+    def combine(x: Future[A], y: Future[A]): Future[A] = for {
+      a <- x
+      b <- y
+    } yield (a |+| b)
+
+    // this combine uses zip which allows parallel computing of the two zipped futures, therefore better than the flatMap above
+    /*
+    def combine(x: Future[A], y: Future[A]): Future[A] = x.zip(y).map(tpl => tpl._1 |+| tpl._2)
+    */
+  }
 
 
-  //Monoid word count
+  // MONOID WORD COUNT
   //Use foldMap with a Monoid to count the number of words, the number of characters and the number of occurences of each word
   //Tip: the Map and Tuple3 Monoid can help
 
   val words: List[String] = rawText.split(" ").toList
 
-
-
-
-
-
+  // 3tuple containing (# words, # characters, occurence counts)
+  val statistics1: (Int, Int, Map[String, Int]) = foldMap(words)(word => (1, word.length, Map(word -> 1)))
+  
   //Now that you have the word count let's extend it with the ability to get the longest word of the text.
   //Tip: Define a Maximum Monoid to do so
+  final case class MaxLenStrings(len:Int, strings: Set[String])
+
+  implicit def maxMonoid: Monoid[MaxLenStrings] = new Monoid[MaxLenStrings] {
+    def empty: MaxLenStrings = MaxLenStrings(0, Set.empty)
+
+    def combine(x: MaxLenStrings, y: MaxLenStrings): MaxLenStrings = 
+      if (x.len > y.len) x
+      else if (y.len > x.len) y
+      else MaxLenStrings(x.len, x.strings union y.strings)
+  }
+
+  // 3 tuple containing (# words, # characters, words with max. length)
+  val statistics2: (Int, Int, MaxLenStrings) = foldMap(words)(word => (1, word.length(), MaxLenStrings(word.length, Set(word))))
 
 
-
-
-
-
-
-  //Functor
+  // FUNCTOR
 
   @typeclass trait Functor[F[_]] {
     def map[A, B](fa: F[A])(f: A => B): F[B]
